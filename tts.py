bfca72f542465df387cd460f391be7725f74e7cb
n0sh4d3 <zablockidawid1@gmail.com>
2025-06-15 21:44:17 +0200

    zabij mnie

--- tts.py
+++ tts.py
@@ -1,10 +1,44 @@
-# this is fucking bs (but works lol)
+from llama_cpp import Llama
 from gtts import gTTS
 import sys
+import argostranslate.package, argostranslate.translate
 
-uster_tts = ""
-for a in sys.argv[1:]:
-    uster_tts += a + " "
+packages = argostranslate.package.get_available_packages()
 
-tts = gTTS(text=uster_tts, lang="pl")
-tts.save("file.mp3")
+
+# fuck this shit
+user_input = " ".join(sys.argv[1:])
+
+pl_en_package = next(p for p in packages if p.from_code == "pl" and p.to_code == "en")
+argostranslate.package.install_from_path(pl_en_package.download())
+
+user_input = argostranslate.translate.translate(user_input, "pl", "en")
+
+llm = Llama(
+    model_path="./model.gguf",
+    n_gpu_layers=-1,  # GPU GPU!!!
+    verbose=False,  # stfu performance output
+)
+
+output = llm(
+    f"Q: {user_input} A: ",
+    max_tokens=64,
+    stop=["Q:"],
+    echo=True,
+)
+
+full_text = output["choices"][0]["text"]
+if "A: " in full_text:
+    response_text = full_text.split("A: ", 1)[1].strip()
+else:
+    response_text = full_text.strip()
+
+pl_en_package = next(p for p in packages if p.from_code == "en" and p.to_code == "pl")
+argostranslate.package.install_from_path(pl_en_package.download())
+
+translated = argostranslate.translate.translate(response_text, "en", "pl")
+print(translated)
+
+tts = gTTS(text=translated, lang="pl")
+tts.save("response.mp3")
+print("zapisano szef")
